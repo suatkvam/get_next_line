e8076334f3046667cba28156c173cee4b2d5fc10
Arif suat Kivam <akivam@k1m43s04.42istanbul.com.tr>
2025-07-04 17:15:09 +0300

    gnl finish

---
+++ get_next_line.h
@@ -0,0 +1,20 @@
+#ifndef GET_NEXT_LINE_H
+# define GET_NEXT_LINE_H
+
+# ifndef BUFFER_SIZE 42
+#  define BUFFER_SIZE 42
+# endif
+
+# include <unistd.h>
+# include <stdlib.h>
+# include <stdio.h>
+# include <fcntl.h>
+
+char	*get_next_line(int fd);
+char	*ft_strchr(const char *s, int c);
+char	*ft_strjoin(char *stash, char *buffer);
+size_t	ft_strlen(const char *s);
+char	*ft_substr(char const *s, unsigned int start, size_t len);
+char	*ft_strdup(const char *s);
+
+#endif
